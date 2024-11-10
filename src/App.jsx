ebe369a64ce95d5890c5f48f83974bef4db3447c
Siddharth Kolipaka <kolipakasiddhu@gmail.com>
2024-11-10 23:39:10 +0530

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
 import Luckydraw from "./components/Luckydraw";
 const App = () => {
-  const location = useLocation();
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/draw",
+      element: <Luckydraw />,
+    },
+  ]);
+
   return (
     <>
-      <Routes location={location} key={location.pathname}>
-        <Route index element={<Home />}></Route>
-        <Route path="/draw" element={<Luckydraw />}></Route>
-      </Routes>
+      <RouterProvider router={router} />
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />}></Route>
-      </Routes>
-    </Router>
+    <App />
   </StrictMode>
 );
